f458832d6ab2b8222175615dbc068efed329cef3
wolf2000-hub <alirezakn@outlook.com>
2020-03-02 00:59:32 -0800

    Test

--- login.php
+++ login.php
@@ -9,6 +9,7 @@ $pass = $_POST['LoginPass'];
 
 $status = "";
 
+
 try {
     $query = "SELECT username, password FROM users WHERE username='" . $username . "' AND password='" . $pass . "'";
     $result = $conn->prepare($query);
