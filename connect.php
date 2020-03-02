1b6ffd238d6a5ce26232fbdb0a5e04248f8b9b0a
Delphi-Coder <47367331+Delphi-Coder@users.noreply.github.com>
2020-03-02 15:15:56 +0330

    Edit connection info

--- connect.php
+++ connect.php
@@ -4,7 +4,7 @@ include "../connect_info.php"; // فایل مربوط به اطالاعات ور
 
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=XmZnnkbVya", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
