28c4137d5a981b6d7cd29a691610283dea61b7eb
Ali Emami <1393aliemami@gmail.com>
2020-03-02 15:01:55 +0330

    edit index
    edit index.php file

--- index.php
+++ index.php
@@ -110,7 +110,6 @@ require "connect.php";
     </div>
 </div>
 
-<p>salam ali hkobi</p>
 <script src="assets/js/jquery-3.4.1.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
 <script src="assets/js/myScripts.js"></script>
