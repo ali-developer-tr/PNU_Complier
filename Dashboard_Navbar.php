5c002ca4fda486194d5f23251854db68a09a16d5
Ali Emami <1393aliemami@gmail.com>
2020-03-03 17:28:57 +0330

    Edit Dashboard

---
+++ Dashboard_Navbar.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-light w-100 dashNav mt-5">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse dashNavList" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link active" href="edit_profile.php">Edit Profile</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="groups.php">Groups</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="submissions.php">Submissions</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contests.php">Contests</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="friend.php">Friends</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<hr>
---
+++ submissions.php
@@ -0,0 +1,10 @@
+<?php
+include "header.php";
+include "connect.php";
+include "Dashboard_Navbar.php"
+?>
+
+    <h1>Submissions!</h1>
+
+<?php
+include "footer.php";
