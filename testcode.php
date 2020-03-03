db5873fb30e506201804e346e5d321af6839dedb
Delphi-Coder <47367331+Delphi-Coder@users.noreply.github.com>
2020-03-03 11:53:50 +0330

    fixed output capture

--- testcode.php
+++ testcode.php
@@ -1,9 +1,7 @@
 <?php
-  //$out=array("","","","");
-  $out="";
+  $out=array();
   $retval=0;
-  //exec("..//JustTest2.exe",$out,$retval);
-  $out=shell_exec("..\JustTest2.exe 2>&1");
-  echo "The return value of process is ".$retval;
-  echo $out[0].$out[1].$out[2].$out[3];
+  exec("E:/PNU-Contest/JustTest2.exe",$out,$retval);
+  echo "The return value of process is ".$retval."<br>";
+  print_r ($out);
 ?>
