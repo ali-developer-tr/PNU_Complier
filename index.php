4c9defcf82a829bec322a19f4194229d1a872842
wolf2000-hub <alirezakn@outlook.com>
2020-03-03 14:58:01 -0800

    index html coming soon

--- index.php
+++ index.php
@@ -1,37 +1,50 @@
 <?php
 include "header.php";
 ?>
-<div class="row">
-    <div class="clearfix">
-        <div class="aslei col-12 col-xl-12 col-lg-12 col-sm-12 col-md-12">
-            <div class="my-text col-12 col-xl-6 col-lg-6 col-sm-12 col-md-12 text-justify mt-2">
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-                laoreet
-                dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper
-                suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit
-                in
-                vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et
-                accumsan et
-                iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla
-                facilisi.
+<div class="aslei col-12 col-sm-12 col-md-12 col-xl-12 col-lg-12">
+    <div class="row">
+        <div class="clearfix">
+            <div class="box1">
+                <div class="my-imag mt-2 ml-4 ">
+                    <img src="assets/img/pnu.png" alt="pnu" id="my-img">
+                </div>
+                <div class="my-text align-top col-12 col-xl-7 col-lg-6 col-sm-12 col-md-12 text-justify mt-2 mb-2">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                    laoreet Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                    laoreet  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                    laoreet v Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                    laoreet
+                </div>
             </div>
-
-                <img src="assets/img/pnuLogo.png" alt="">
-
+        </div>
+        <div class="row">
+            <div class="container">
+                <div class="box2 col-12 col-lg-6 col-sm-12 col-md-12 col-xl-6 mt-3">
+                    <div class="my-imag mt-2 ">
+                        <img src="assets/img/image2.png" alt="pnu" id="my-img1" width="170px" height="250px">
+                    </div>
+                    <div class="my-text align-top col-12 col-xl-6 col-lg-6 col-sm-12 col-md-12 text-justify mt-2 mb-2">
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+                        tincidunt ut
+                        laoreet
+                    </div>
+                </div>
+                <div class="box3 col-12 col-lg-5 col-sm-12 col-md-12 col-xl-5 mt-3">
+                    <div class="my-imag mt-2">
+                        <img src="assets/img/image2.png" alt="pnu" id="my-img2" width="170px" height="250px">
+                    </div>
+                    <div class="my-text align-top col-12 col-xl-6 col-lg-6 col-sm-12 col-md-12 text-justify mt-2 mb-2">
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+                        tincidunt ut
+                        laoreet
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
-<div class="myForm mt-5">
-    <form class="form-group" action="" method="post">
-        <label class="form-check-label mb-3" for="myCode">Code: </label>
-        <textarea class="codeArea form-control shadow-sm" name="code" id="myCode" cols="30" rows="10">
-            </textarea>
-        <div class="text-right w-100">
-            <button type="submit" class="mt-4 btnSubmit dark-primary-color">Submit Code <i
-                        class="fa fa-angle-double-right"></i></button>
-        </div>
-    </form>
-</div>
+
+
 
 <?php
 include "footer.php";
