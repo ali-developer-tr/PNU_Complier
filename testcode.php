692d4c0f64348a02a537428b6e54b0dcf088c739
Delphi-Coder <47367331+Delphi-Coder@users.noreply.github.com>
2020-03-03 14:25:14 +0330

    awesome

--- testcode.php
+++ testcode.php
@@ -1,7 +1,21 @@
 <?php
+  function compilecpp_gplusplus($infile,int &$ErrCode,string &$ErrStr)
+  {
+    $out=array();
+    $ErrCode = 4;
+    exec("E:/PNU-Contest/JustTest2.exe an_argument",$out,$ErrStr);
+    
+    return 0;
+  }
   $out=array();
   $retval=0;
-  exec("E:/PNU-Contest/JustTest2.exe",$out,$retval);
+  exec("E:/PNU-Contest/JustTest2.exe an_argument",$out,$retval);
   echo "The return value of process is ".$retval."<br>";
-  print_r ($out);
+  var_dump ($out);
+  echo "<br>";
+  $erc=0;
+  $ers="";
+  compilecpp_gplusplus("c:\1.exe",$erc,$ers);
+  echo $erc."<br>";
+  echo $ers."<br>";
 ?>
