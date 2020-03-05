7d135bc6c0c04ddba0747a4701154523da4262b8
Ali Emami <1393aliemami@gmail.com>
2020-03-05 21:36:14 +0330

    dashboard changed

--- dashboard.php
+++ dashboard.php
@@ -9,6 +9,32 @@ if (($item > 5) || ($item < 0)) {
 include "header.php";
 include "connect.php";
 include "Dashboard_Navbar.php";
+
+$details = array();
+$username = $_SESSION['username'];
+$q = "SELECT * FROM tblusers WHERE username=:username";
+$result = $conn->prepare($q);
+$result->bindParam(':username', $username);
+$result->execute();
+
+$row = $result->fetch(PDO::FETCH_ASSOC);
+
+$details = array(
+    "firstname" => $row['firstname'],
+    "lastname" => $row['lastname'],
+    "username" => $row['username'],
+    "email" => $row['email'],
+    "photo" => $row['photo'],
+    "birthday" => $row['birthday'],
+    "bio" => $row['bio'],
+    "registertime" => $row['registertime'],
+    "score" => $row['score'],
+    "languages" => $row['languages'],
+    "namechangecount" => $row['namechangecount'],
+    "privacy" => $row['privacy']
+);
+
+
 switch ($item) {
     case 1:
         include "edit_profile.php";
@@ -30,54 +56,67 @@ switch ($item) {
 
 if ($item == 0) {
     ?>
+    <div class="dash-main">
+        <div class="row">
+            <div class="col-6">
+                <div class="dash-left shadow p-4">
+                    <h3><i class="fa fa-info"></i> Your details</h3>
+                    <hr>
+                    <ul>
+                        <li>
+                            <span><i class="fa fa-user-circle"></i> Full Name: </span>
+                            <span><?php echo $details['firstname'] ?><?php echo $details['lastname'] ?></span>
+                        </li>
+                        <li>
+                            <span><i class="fa fa-user"></i> Username: </span>
+                            <span><?php echo $details['username']; ?></span>
+                        </li>
+                        <li>
+                            <span><i class="fa fa-envelope"></i> Email: </span>
+                            <span><?php echo $details['email']; ?></span>
+                        </li>
+                        <li>
+                            <span><i class="fa fa-birthday-cake"></i> Birthday: </span>
+                            <span><?php echo $details['birthday']; ?></span>
+                        </li>
+                        <li>
+                            <span><i class="fa fa-calendar-check"></i> Register time: </span>
+                            <span><?php echo date('Y-m-d', strtotime(str_replace('-', '/', $details['registertime']))); ?></span>
+                        </li>
+                        <li>
+                            <span><i class="fa fa-star"></i> Score: </span>
+                            <span><?php echo $details['score']; ?></span>
+                        </li>
+                        <li>
+                            <span><i class="fa fa-code"></i> Languages: </span>
+                            <span><?php echo $details["languages"]; ?></span>
+                        </li>
+                        <li>
+                            <span><i class="fa fa-user-lock"></i></span>
+                            <div class="checkbox disabled d-inline-block">
+                                <label><input type="checkbox" value="" disabled checked> Option 3</label>
+                                <label><input type="checkbox" value="" disabled checked> Option 3</label>
+                            </div>
 
-    <?php
-    $details = array();
-    $username = $_SESSION['username'];
-    $q = "SELECT * FROM tblusers WHERE username=:username";
-    $result = $conn->prepare($q);
-    $result->bindParam(':username', $username);
-    $result->execute();
+                                <?php
+                                $No=$details["privacy"];
+                                // privacy options : int32
+                                // bit 0:
+                                //if ()
+                                ?>
 
-    $row = $result->fetch(PDO::FETCH_ASSOC);
-
-    $details = array(
-        "firstname" => $row['firstname'],
-        "lastname" => $row['lastname'],
-        "username" => $row['username'],
-        "email" => $row['email'],
-        "photo" => $row['photo'],
-        "birthday" => $row['birthday'],
-        "bio" => $row['bio'],
-        "score" => $row['score'],
-        "languages" => $row['languages'],
-        "namechangecount" => $row['namechangecount'],
-        "privacy" => $row['privacy']
-    );
-
-
-    ?>
-
-    <h1>Dashboard!</h1>
-    <hr>
-    <div class="row">
-        <div class="col-6">
-            <div class="dash-left shadow p-4">
-                <ul>
-                    <li>
-                        <span>Name: </span>
-                        <span><?php echo $details['firstname']?></span>
-                    </li>
-                </ul>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-        <div class="col-6">
-            <div class="dash-right">
-                <ul>
-                    <li>
-                        <span></span>
-                    </li>
-                </ul>
+            <div class="col-6">
+                <div class="dash-right">
+                    <ul>
+                        <li>
+                            <span></span>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
