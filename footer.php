18b0d057779af2874249f8e3ee268bbea8da4d5f
wolf2000-hub <alirezakn@outlook.com>
2020-03-05 15:26:53 -0800

    footer icon create

--- footer.php
+++ footer.php
@@ -4,34 +4,54 @@
     <div>
         <!--Contact Section-->
         <div class="container bg-grey">
-            <h2 class="text-center p-3">CONTACT</h2>
-            <div class="row">
-                <div class="col-sm-5">
-                    <ul class="footer-li">
-                        <li class="mt-4 text-justify"><span><i class="fa fa-info-circle"></i> We will announce your response within the next 24 hours</span></li>
-                        <li class="mt-4"><span><i class="fa fa-address-card"></i> East Azarbaijan-Tabriz</span></li>
-                        <li class="mt-4"><a href="tel:3344558822"><i class="fa fa-phone"></i> 3344558822</a></li>
-                        <li class="mt-4"><a href="mailto:alirezakn@outlook.com"><i class="fa fa-envelope"></i> alirezakn@outlook.com</a></li>
-                    </ul>
+            <div class="my-footr-nav ">
+                <ul class="text-center ">
+                    <li class="ml-2"><a href="#"><i class="fa fa-home"></i> home</a></li>
+                    <li class="ml-2"><a href="#"><i class="fa fa-chart-bar"></i> Contests</a></li>
+                    <li class="ml-2"><a href="#"><i class="fa fa-info-circle"></i>About us</a></li>
+                    <li class="ml-2"><a href="#"><i class="fa fa-envelope-open-text"></i> Contact us</a></li>
+                </ul>
+            </div>
+            <div>
+                <h2 class="text-center p-3">CONTACT</h2>
+                <div class="row">
+                    <div class="col-sm-5">
+                        <ul class="footer-li">
+                            <li class="mt-4 text-justify"><span><i class="fa fa-info-circle"></i> We will announce your response within the next 24 hours</span>
+                            </li>
+                            <li class="mt-4"><span><i class="fa fa-address-card"></i> East Azarbaijan-Tabriz</span></li>
+                            <li class="mt-4" id="li-a1"><a href="tel:3344558822"><i class="fa fa-phone"></i> 3344558822</a></li>
+                            <li class="mt-4" id="li-a2"><a href="mailto:alirezakn@outlook.com"><i class="fa fa-envelope"></i>
+                                    alirezakn@outlook.com</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-7">
+                        <div class="row">
+                            <div class="col-sm-6 form-group">
+                                <input class="form-control" id="name" name="name" placeholder="Name" type="text"
+                                       required>
+                            </div>
+                            <div class="col-sm-6 form-group">
+                                <input class="form-control" id="email" name="email" placeholder="Email" type="email"
+                                       required>
+                            </div>
+                        </div>
+                        <label for="comments"></label><textarea class="form-control" id="comments" name="comments"
+                                                                placeholder="Comment" rows="5"></textarea><br>
+                        <div class="row">
+                            <div class="col-sm-12 form-group">
+                                <button class="btn btn-default pull-right shadow-sm" type="submit" id="btn-sub">Send</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-sm-7">
-                    <div class="row">
-                        <div class="col-sm-6 form-group">
-                            <input class="form-control" id="name" name="name" placeholder="Name" type="text"
-                                   required>
-                        </div>
-                        <div class="col-sm-6 form-group">
-                            <input class="form-control" id="email" name="email" placeholder="Email" type="email"
-                                   required>
-                        </div>
-                    </div>
-                    <label for="comments"></label><textarea class="form-control" id="comments" name="comments"
-                                                            placeholder="Comment" rows="5"></textarea><br>
-                    <div class="row">
-                        <div class="col-sm-12 form-group">
-                            <button class="btn btn-default pull-right text-white" type="submit">Send</button>
-                        </div>
-                    </div>
+                <div>
+                    <ul class="icon ">
+                        <li><i class="fab fa-facebook-f"></i></li>
+                        <li class="ml-5"><i class="fab fa-twitter"></i></li>
+                        <li class="ml-5"><i class="fab fa-skype"></i></li>
+                        <li class="ml-5"><i class="fab fa-pinterest-p"></i></li>
+                    </ul>
                 </div>
             </div>
         </div>
