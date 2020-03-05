d13123af62eb43db5b593c125b1b63b6ebb7a104
wolf2000-hub <alirezakn@outlook.com>
2020-03-05 08:10:02 -0800

    footer-header

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include "header.php";
 <div class="aslei col-12 col-sm-12 col-md-12 col-xl-12 col-lg-12">
     <div class="row">
         <div class="clearfix">
-            <div class="box1">
+            <div class="box1 shadow">
                 <div class="my-imag mt-2 ml-4 ">
                     <img src="assets/img/pnu.png" alt="pnu" id="my-img">
                 </div>
@@ -22,7 +22,7 @@ include "header.php";
         </div>
         <div class="row">
             <div class="container">
-                <div class="box2 col-12 col-lg-5 col-sm-12 col-md-12 col-xl-5 mt-3 w-100">
+                <div class="box2 col-12 col-lg-5 col-sm-12 col-md-12 col-xl-5 mt-3 shadow w-100">
                     <div class="my-imag mt-2 ">
                         <img src="assets/img/image2.png" alt="pnu" id="my-img1" width="170px" height="250px">
                     </div>
@@ -32,7 +32,7 @@ include "header.php";
                         laoreet
                     </div>
                 </div>
-                <div class="box3 col-12 col-lg-6 col-sm-12 col-md-12 col-xl-6 mt-3  w-100">
+                <div class="box3 col-12 col-lg-6 col-sm-12 col-md-12 col-xl-6 mt-3 shadow  w-100">
                     <div class="my-imag mt-2">
                         <img src="assets/img/image2.png" alt="pnu" id="my-img2" width="170px" height="250px">
                     </div>
@@ -45,21 +45,20 @@ include "header.php";
             </div>
         </div>
     </div>
-</div>
-<div class="row">
-    <div class="col-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 mt-3 w-100">
-        <div class="my-news">
-            <div class="my-imag">
-                <img src="assets/img/55.jpg" alt="not-found">
-            </div>
-            <div class="my-text align-top col-12 col-xl-6 col-lg-6 col-sm-12 col-md-12 text-justify mt-2 mb-2">
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
-                tincidunt ut
-                laoreet
+    <div class="row">
+        <div class="col-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 mt-3 w-100 shadow">
+            <div class="my-news">
+                <div class="my-imag">
+                    <img src="assets/img/55.jpg" alt="not-found">
+                </div>
+                <div class="my-text align-top col-12 col-xl-6 col-lg-6 col-sm-12 col-md-12 text-justify mt-2 mb-2">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+                    tincidunt ut
+                    laoreet
+                </div>
             </div>
         </div>
     </div>
-</div>
-<?php
-include "footer.php";
-?>
+    <?php
+    include "footer.php";
+    ?>
